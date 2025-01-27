6db317acf010cd52abadf28aff02f7d0c32c21fd
Idemudia-Precious <idemudiapreciousosaze@gmail.com>
2025-01-27 15:38:00 +0100

    fix deployment error

--- app/(root)/podcasts/[podcastId]/page.tsx
+++ app/(root)/podcasts/[podcastId]/page.tsx
@@ -1,92 +1,129 @@
-"use client"
+/* eslint-disable no-unused-vars */
 
-import EmptyState from '@/components/EmptyState'
-import LoaderSpinner from '@/components/LoaderSpinner'
-import PodcastCard from '@/components/PodcastCard' 
-import PodcastDetailPlayer from '@/components/PodcastDetailPlayer'
-import { api } from '@/convex/_generated/api'
-import { Id } from '@/convex/_generated/dataModel'
-import { useUser } from '@clerk/nextjs'
-import { useQuery } from 'convex/react'
-import Image from 'next/image'
-import React from 'react'
+import { Dispatch, SetStateAction } from "react";
 
-const PodcastDetails = ({ params: { podcastId } }: { params: { podcastId: Id<'podcasts'> }}) => {
+import { Id } from "@/convex/_generated/dataModel";
 
-  const { user } = useUser();
-
-  const podcast = useQuery(api.podcasts.getPodcastById, { podcastId })
-
-  const similarPodcasts = useQuery(api.podcasts.getPodcastByVoiceType, { podcastId })
-
-  const isOwner = user?.id === podcast?.authorId;
-
-  if(!similarPodcasts || !podcast) return <LoaderSpinner />
-
-  return (
-    <section className='flex w-full flex-col'>
-      <header className='mt-9 flex items-center justify-between'>
-        <h1 className='text-20 font-bold text-white-1'>
-          Currently Playing
-        </h1>
-        <figure className='flex gap-3'>
-          <Image 
-            src="/icons/headphone.svg"
-            width={24}
-            height={24}
-            alt="headphone"
-          />
-          <h2 className='text-16 font-bold text-white-1'>{podcast?.views}</h2>
-        </figure>
-      </header>
-
-      <PodcastDetailPlayer
-        isOwner={isOwner}
-        podcastId={podcast._id}
-        {...podcast}
-      />
-
-      <p className="text-white-2 text-16 pb-8 pt-[45px] font-medium max-md:text-center">{podcast?.podcastDescription}</p>
-
-      <div className="flex flex-col gap-8">
-        <div className='flex flex-col gap-4'>
-          <h1 className='text-18 font-bold text-white-1'>Transcription</h1>
-          <p className="text-16 font-medium text-white-2">{podcast?.voicePrompt}</p>
-        </div>
-        <div className='flex flex-col gap-4'>
-          <h1 className='text-18 font-bold text-white-1'>Thumbnail Prompt</h1>
-          <p className="text-16 font-medium text-white-2">{podcast?.imagePrompt}</p>
-        </div>
-      </div>
-
-      <section className="mt-8 flex flex-col gap-5">
-        <h1 className="text-20 font-bold text-white-1">Similar Podcasts</h1>
-
-        {similarPodcasts && similarPodcasts.length > 0 ? (
-          <div className="podcast_grid">
-            {similarPodcasts?.map(({ _id, podcastTitle, podcastDescription, imageUrl }) => (
-              <PodcastCard 
-                key={_id}
-                imgUrl={imageUrl as string}
-                title={podcastTitle}
-                description={podcastDescription}
-                podcastId={_id}
-              />
-            ))}
-          </div>
-        ) : (
-          <> 
-            <EmptyState 
-              title="No similar podcasts found"
-              buttonLink="/discover"
-              buttonText="Discover more podcasts"
-            />
-          </>
-        )}
-      </section>
-
-    </section>
-  )
+export interface EmptyStateProps {
+  title: string;
+  search?: boolean;
+  buttonText?: string;
+  buttonLink?: string;
 }
 
-export default PodcastDetails
+export interface TopPodcastersProps {
+  _id: Id<"users">;
+  _creationTime: number;
+  email: string;
+  imageUrl: string;
+  clerkId: string;
+  name: string;
+  podcast: {
+    podcastTitle: string;
+    podcastId: Id<"podcasts">;
+  }[];
+  totalPodcasts: number;
+}
+
+export interface PodcastProps {
+  _id: Id<"podcasts">;
+  _creationTime: number;
+  audioStorageId: Id<"_storage"> | null;
+  user: Id<"users">;
+  podcastTitle: string;
+  podcastDescription: string;
+  audioUrl: string | null;
+  imageUrl: string | null;
+  imageStorageId: Id<"_storage"> | null;
+  author: string;
+  authorId: string;
+  authorImageUrl: string;
+  voicePrompt: string;
+  imagePrompt: string | null;
+  voiceType: string;
+  audioDuration: number;
+  views: number;
+}
+
+export interface ProfilePodcastProps {
+  podcasts: PodcastProps[];
+  listeners: number;
+}
+
+export interface GeneratePodcastProps {
+  voiceType: string;
+  setAudio: Dispatch<SetStateAction<string>>;
+  audio: string;
+  setAudioStorageId: Dispatch<SetStateAction<Id<"_storage"> | null>>;
+  voicePrompt: string;
+  setVoicePrompt: Dispatch<SetStateAction<string>>;
+  setAudioDuration: Dispatch<SetStateAction<number>>;
+}
+
+export interface GenerateThumbnailProps {
+  setImage: Dispatch<SetStateAction<string>>;
+  setImageStorageId: Dispatch<SetStateAction<Id<"_storage"> | null>>;
+  image: string;
+  imagePrompt: string;
+  setImagePrompt: Dispatch<SetStateAction<string>>;
+}
+
+export interface LatestPodcastCardProps {
+  imgUrl: string;
+  title: string;
+  duration: string;
+  index: number;
+  audioUrl: string;
+  author: string;
+  views: number;
+  podcastId: Id<"podcasts">;
+}
+
+export interface PodcastDetailPlayerProps {
+  audioUrl: string;
+  podcastTitle: string;
+  author: string;
+  isOwner: boolean;
+  imageUrl: string;
+  podcastId: Id<"podcasts">;
+  imageStorageId: Id<"_storage">;
+  audioStorageId: Id<"_storage">;
+  authorImageUrl: string;
+  authorId: string;
+}
+
+export interface AudioProps {
+  title: string;
+  audioUrl: string;
+  author: string;
+  imageUrl: string;
+  podcastId: string;
+}
+
+export interface AudioContextType {
+  audio: AudioProps | undefined;
+  setAudio: React.Dispatch<React.SetStateAction<AudioProps | undefined>>;
+}
+
+export interface PodcastCardProps {
+  imgUrl: string;
+  title: string;
+  description: string;
+  podcastId: Id<"podcasts">;
+}
+
+export interface CarouselProps {
+  fansLikeDetail: TopPodcastersProps[];
+}
+
+export interface ProfileCardProps {
+  podcastData: ProfilePodcastProps;
+  imageUrl: string;
+  userFirstName: string;
+}
+
+export type UseDotButtonType = {
+  selectedIndex: number;
+  scrollSnaps: number[];
+  onDotButtonClick: (index: number) => void;
+};
